780390410c78b9150fa7fe6dcd2e14cbc0966b77
johnDorian <jlessels@gmail.com>
2016-03-03 17:41:26 +0100

    first version up and running

--- server.R
+++ server.R
@@ -6,18 +6,73 @@
 #
 
 library(shiny)
+library(lubridate)
+library(dplyr)
+library(ggplot2)
+library(reshape2)
+load("data/race_speed.Rdata")
 
 shinyServer(function(input, output) {
-
-  output$distPlot <- renderPlot({
-
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+  
+  output$finishing_times <- renderPlot({
+    pred_race_results <- estimate_finishing_times(input$participants_1, input$distance_1, input$average_1, 
+                                                  input$sd_1, 
+                                                  input$starting_times_1[1], input$starting_times_1[2])
+    pred_race_results$variable <- "race1"
+    pred_race_results_2 <- estimate_finishing_times(input$participants_2, input$distance_2, input$average_2, 
+                                                    input$sd_2, 
+                                                    input$starting_times_2[1], input$starting_times_2[2])
+    pred_race_results_2$variable <- "race2"
+      full_join(pred_race_results, pred_race_results_2) %>% 
+      ggplot() + geom_point(aes(dt, place, colour = variable), size = 3) +
+      theme_bw(20) + xlab("Time") + ylab("Contestant placing") + 
+      scale_colour_discrete("Race", labels = c("100 km", "100 mile"))
 
   })
-
+  output$finishing_freq <- renderPlot({
+    pred_race_results <- estimate_finishing_times(input$participants_1, input$distance_1, input$average_1, 
+                                                  input$sd_1, 
+                                                  input$starting_times_1[1], input$starting_times_1[2])
+    pred_race_results$variable <- "race1"
+    pred_race_results_2 <- estimate_finishing_times(input$participants_2, input$distance_2, input$average_2, 
+                                                    input$sd_2, 
+                                                    input$starting_times_2[1], input$starting_times_2[2])
+    pred_race_results_2$variable <- "race2"
+    
+    total_line_crosses <- over_the_line(pred_race_results, pred_race_results_2)
+  reshape2::melt(total_line_crosses[,1:3], id =  c("dt")) %>% 
+    ggplot() + geom_bar(aes(dt, value, fill = variable), 
+                        size = 1, stat = "identity") + 
+    theme_bw(20) + xlab("Time") + ylab("Finishers (people/ 10 min)") + 
+    scale_fill_discrete("Race", labels = c("100 km", "100 mile"))
+  
 })
+})
+
+
+estimate_finishing_times <- function(participants, race_length, average_speed,
+                                     sd_speed, release_start, release_end) {
+  
+  speed <- rnorm(participants, average_speed, sd_speed)
+  leaving_times <- sample(
+    seq(release_start, release_end, by = "10 min"), 
+    participants, replace = TRUE)
+  finishing_time <- leaving_times + (race_length/speed)*60*60
+  data.frame(dt = sort(finishing_time), place = 1:participants)
+}
+
+over_the_line <- function(race1, race2) {
+  race1_result <- race1 %>% 
+    group_by(dt = dt - (minute(dt)%%(10)*60+second(dt))) %>% 
+    summarise(race1 = n())
+  race2_result <- race2 %>% 
+    group_by(dt = dt - (minute(dt)%%(10)*60+second(dt))) %>% 
+    summarise(race2 = n())
+  all_finishers <- full_join(race1_result, race2_result, by = "dt") %>% 
+    mutate(race1 = ifelse(is.na(race1), 0 , race1), 
+           race2 = ifelse(is.na(race2), 0 , race2),
+           total = race1 + race2) %>% 
+    arrange(dt)
+  all_finishers
+}
+
--- ui.R
+++ ui.R
@@ -6,25 +6,83 @@
 #
 
 library(shiny)
+load("data/race_speed.Rdata")
 
 shinyUI(fluidPage(
-
+#   tags$head(
+#     tags$link(rel = "stylesheet", type = "text/css", href = "style.css")
+#   ),
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel(""),
 
   # Sidebar with a slider input for number of bins
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
+  fluidRow(
+    column(3,
+    wellPanel(HTML("<h3>Race 1 options</h3>"),
+      sliderInput("participants_1",
+                  "Number of riders:",
+                  min = 100,
+                  max = 500,
+                  value = 250),
+      numericInput("distance_1", 'Race distance (km):', 100,
+                   min = 50, max = 200),
+      numericInput("average_1", 
+                   'Average Speed (km/h):', 
+                   round(race_speed$average[1]), 
+                   step = 1,
+                   min = 5, max = 50),
+      numericInput("sd_1", 
+                   'Standard deviation  of Speed (km/h):', 
+                   round(race_speed$sd[1], 2), 
+                   step = 0.01,
+                   min = 1, max = 10),
+      sliderInput("starting_times_1",
+                  "Start and end of race start:",
+                  min = as.POSIXlt("2016-05-01 07:00:00", tz = "UTC"),
+                  max = as.POSIXlt("2016-05-01 12:00:00", tz = "UTC"),
+                  step = 600,
+                  value = as.POSIXlt(c("2016-05-01 08:30:00", "2016-05-01 09:30:00"), tz = "UTC"), 
+                  timeFormat = "%T",
+                  timezone = "UTC")
     ),
+    wellPanel(HTML("<h3>Race 2 options</h3>"),
+      sliderInput("participants_2",
+                  "Number of riders:",
+                  min = 100,
+                  max = 500,
+                  value = 250),
+      numericInput("distance_2", 'Race distance (km):', 160,
+                   min = 50, max = 200),
+      numericInput("average_2", 
+                   'Average Speed (km/h):', 
+                   round(race_speed$average[2]), 
+                   step = 1,
+                   min = 5, max = 50),
+      numericInput("sd_2", 
+                   'Standard deviation  of Speed (km/h):', 
+                   round(race_speed$sd[2], 2), 
+                   step = 0.01,
+                   min = 1, max = 10),
+      sliderInput("starting_times_2",
+                  "Start and end of race start:",
+                  min = as.POSIXlt("2016-05-01 07:00:00", tz = "UTC"),
+                  max = as.POSIXlt("2016-05-01 12:00:00", tz = "UTC"),
+                  step = 600,
+                  value = as.POSIXlt(c("2016-05-01 08:30:00", "2016-05-01 09:30:00"), tz = "UTC"), 
+                  timeFormat = "%T",
+                  timezone = "UTC")
+    )
+    ),
+    
 
     # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("distPlot")
-    )
+  column(9,
+         HTML("<h1>Estimated 2016 KOM Finishing Times</h1>"),
+    plotOutput("finishing_times", width = "80%"),
+    HTML("<h1>Estimated 2016 KOM Finishing Frequency</h1>"),
+    plotOutput("finishing_freq", width = "80%")
+    
   )
-))
+  )
+)
+)
